38b29c6d9099eb2e4db0501ff4ea087dde62842c
Robert French <rfrench@rfrench.org>
2023-02-04 21:07:12 -0800

    Initial checkin

---
+++ read_csv.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+
+YEAR = 2022
+
+def _read_pvwatts_file(filename):
+    header_types = {
+        "Month": np.int32,
+        "Day": np.int32,
+        "Hour": np.int32,
+        "DC Array Output (W)": np.float64,
+        "AC System Output (W)": np.float64
+    }
+
+    df = pd.read_csv(filename, skiprows=31, dtype=header_types)
+    df = df.set_index(["Month", "Day", "Hour"])
+    df = df.filter(["DC Array Output (W)"])
+    df.rename(columns={"DC Array Output (W)": "DCW"}, inplace=True)
+
+    return df
+
+def read_pv_data():
+    # Output from NREL PVWatts Calculator (https://pvwatts.nrel.gov/pvwatts.php)
+    # "Month","Day","Hour","Beam Irradiance (W/m2)","Diffuse Irradiance (W/m2)",
+    # "Ambient Temperature (C)","Wind Speed (m/s)","Albedo",
+    # "Plane of Array Irradiance (kW/m2)","Cell Temperature (C)",
+    # "DC Array Output (W)","AC System Output (W)"
+
+    df_east = _read_pvwatts_file("data/pvwatts_hourly_east.csv")
+    df_south = _read_pvwatts_file("data/pvwatts_hourly_south.csv")
+    df_west = _read_pvwatts_file("data/pvwatts_hourly_west.csv")
+    df_north = _read_pvwatts_file("data/pvwatts_hourly_north.csv")
+
+    df = df_east.join(df_south, lsuffix="_E", rsuffix="_S")
+    df_west.rename(columns={"DCW": "DCW_W"}, inplace=True)
+    df = df.join(df_west)
+    df_north.rename(columns={"DCW": "DCW_N"}, inplace=True)
+    df = df.join(df_north)
+
+    return df
+
+def read_usage_data():
+    # DateTime,Device ID,Name,Device Type,Device Make,Device Model,
+    # Device Location,Avg Wattage,kWh
+
+    header_types = {
+        "Avg Wattage": np.float64
+    }
+
+    df = pd.read_csv("data/usage_2022.csv", skiprows=1, dtype=header_types)
+    df["Month"] = [int(x[5:7]) for x in df["DateTime"]]
+    df["Day"] = [int(x[8:10]) for x in df["DateTime"]]
+    df["Hour"] = [int(x[11:13]) for x in df["DateTime"]]
+    df = df.set_index(["Month", "Day", "Hour"])
+    df_usage = df[df["Name"] == "Total Usage"].copy()
+    df_usage.rename(columns={"Avg Wattage": "Used_ACKW"}, inplace=True)
+    df_solar = df[df["Name"] == "Solar Production"].copy()
+    df_solar["Avg Wattage"] = -df_solar["Avg Wattage"]
+    df_solar.rename(columns={"Avg Wattage": "Total_Gen_Cur_ACKW"}, inplace=True)
+    df = df_usage.join(df_solar, lsuffix="_1", rsuffix="_2")
+    df = df.filter(["Used_ACKW", "Total_Gen_Cur_ACKW"])
+    df["Used_ACKW"] = df["Used_ACKW"] / 1000
+    df["Total_Gen_Cur_ACKW"] = df["Total_Gen_Cur_ACKW"] / 1000
+
+    return df
---
+++ solar.py
@@ -0,0 +1,235 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from read_csv import read_pv_data, read_usage_data
+
+DEFAULT_INVERTER_EFFICIENCY = 0.99
+DEFAULT_PANEL_WATTS = 1000 # In the analysis files
+
+BASELINE_RATE = 0.09
+BASELINE_ALLOWANCE = 810.82 # monthly
+
+inverter_type = "current"
+# inverter_type = "solaredge"
+# inverter_type = "enphase"
+
+# panel_type = "none"
+panel_type = "current"
+# panel_type = "starpower"
+# panel_type = "earthelectric"
+# panel_type = "svce100_bid3"
+# panel_type = "svce100_3300_bid3"
+
+tou_type = "NEM2-TOUC"
+# tou_type = "NEM2-TOUD"
+# tou_type = "NEM3-TOUC"
+# tou_type = "NEM3-TOUD"
+
+# EV_MILES = 0
+EV_MILES = 10000
+EV_POWER_PER_MILE = 0.346
+EV_ANNUAL_POWER = EV_MILES * EV_POWER_PER_MILE
+add_daily_ev_amount = EV_ANNUAL_POWER / 365
+
+BATTERY_CAPACITY = 10 # Useable kWh
+
+PANEL_LAYOUTS = {
+    "none": {
+        "panels": [],
+        "total_loss": 0.00,
+    },
+    "current": {
+        "panels": [
+            {"panel_watts": 160, "direction": "south", "number":  6, "shading": 0.00},
+            {"panel_watts": 160, "direction":  "west", "number": 29, "shading": 0.00},
+        ],
+        "total_loss": 0.15,
+    },
+    "starpower": {
+        "panels": [
+            {"panel_watts": 400, "direction":  "east", "number":  7, "shading": 0.00},
+            {"panel_watts": 400, "direction": "south", "number": 19, "shading": 0.00},
+            {"panel_watts": 400, "direction":  "west", "number": 20, "shading": 0.00},
+        ],
+        "total_loss": 0.15,
+    },
+    "earthelectric": {
+        "panels": [
+            {"panel_watts": 410, "direction":  "east", "number":  3, "shading": 0.00},
+            {"panel_watts": 410, "direction": "south", "number": 12, "shading": 0.00},
+            {"panel_watts": 410, "direction":  "west", "number": 29, "shading": 0.00},
+        ],
+        "total_loss": 0.15,
+    },
+    "svce100_bid3": {
+        "panels": [
+            {"panel_watts": 400, "direction":  "east", "number":  6, "shading": 0.00},
+            {"panel_watts": 400, "direction": "south", "number": 11, "shading": 0.00},
+            {"panel_watts": 400, "direction":  "west", "number": 19, "shading": 0.00},
+            {"panel_watts": 400, "direction": "north", "number":  9, "shading": 0.00},
+        ],
+        "total_loss": 0.22,
+    },
+    "svce100_3300_bid3": {
+        "panels": [
+            {"panel_watts": 400, "direction":  "east", "number":  6, "shading": 0.00},
+            {"panel_watts": 400, "direction": "south", "number": 11, "shading": 0.00},
+            {"panel_watts": 400, "direction":  "west", "number": 19, "shading": 0.00},
+            {"panel_watts": 400, "direction": "north", "number": 12, "shading": 0.00},
+        ],
+        "total_loss": 0.22,
+    }
+}
+
+INVERTERS = {
+    "current": {
+        "efficiency": 0.9,
+        "fudge_factor": 0.55,
+        "ac_max": 1e38
+    },
+    "solaredge": {
+        "efficiency": 0.99 * 0.988, # inverter, power optimizers
+        "fudge_factor": 0.8715,
+        "ac_max": 11400
+    },
+    "enphase": {
+        "efficiency": 0.92,
+        "fudge_factor": 0.89,
+        "ac_max": 1e38
+    },
+}
+
+df_pv = read_pv_data()
+df_usage = read_usage_data()
+
+df_usage.loc[pd.IndexSlice[:, :, 1], "Used_ACKW"] += add_daily_ev_amount
+
+panel_layout = PANEL_LAYOUTS[panel_type]
+inverter_details = INVERTERS[inverter_type]
+
+total_dckw = 0
+rated_dckw = 0
+for panel_details in panel_layout["panels"]:
+    dckw = (panel_details["panel_watts"] / DEFAULT_PANEL_WATTS  *
+            panel_details["number"])
+    rated_dckw += dckw
+    total_dckw += (dckw * df_pv["DCW_"+panel_details["direction"][0].upper()] /
+                   1000)
+
+df_pv["Total_DCKW"] = total_dckw
+
+df_pv["Total_Gen_ACKW"] = ((df_pv["Total_DCKW"] *
+                            (DEFAULT_INVERTER_EFFICIENCY /
+                             inverter_details["efficiency"]) *
+                            (1-panel_layout["total_loss"]) *
+                            inverter_details["fudge_factor"])
+                           .clip(0, inverter_details["ac_max"]))
+
+df = df_pv.join(df_usage)
+
+df["Net_Usage"] = df["Used_ACKW"] - df["Total_Gen_ACKW"]
+
+df["Sell_Price"] = 0
+df["Buy_Price"] = 0
+if tou_type == "NEM2-TOUC" or tou_type == "NEM3-TOUC":
+    winter_off_peak_index = pd.IndexSlice[[1, 2, 3, 4, 5, 10, 11, 12], :,
+                                          [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,21,22,23]]
+    df.loc[winter_off_peak_index, "Sell_Price"] = 0.37 if tou_type == "NEM2-TOUC" else 0.08
+    df.loc[winter_off_peak_index, "Buy_Price"] = 0.37
+    winter_on_peak_index = pd.IndexSlice[[1, 2, 3, 4, 5, 10, 11, 12], :,
+                                         [16,17,18,19,20]]
+    df.loc[winter_on_peak_index, "Sell_Price"] = 0.39 if tou_type == "NEM2-TOUC" else 0.08
+    df.loc[winter_on_peak_index, "Buy_Price"] = 0.39
+    summer_off_peak_index = pd.IndexSlice[[6, 7, 8, 9], :,
+                                          [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,21,22,23]]
+    df.loc[summer_off_peak_index, "Sell_Price"] = 0.43 if tou_type == "NEM2-TOUC" else 0.08
+    df.loc[summer_off_peak_index, "Buy_Price"] = 0.43
+    summer_on_peak_index = pd.IndexSlice[[6, 7, 8, 9], :,
+                                         [16,17,18,19,20]]
+    df.loc[summer_on_peak_index, "Sell_Price"] = 0.49 if tou_type == "NEM2-TOUC" else 0.08
+    df.loc[summer_on_peak_index, "Buy_Price"] = 0.49
+elif tou_type == "NEM2-TOUD" or tou_type == "NEM3-TOUD":
+    winter_off_peak_index = pd.IndexSlice[[1, 2, 3, 4, 5, 10, 11, 12], :,
+                                          [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,20,21,22,23]]
+    df.loc[winter_off_peak_index, "Sell_Price"] = 0.37 if tou_type == "NEM2-TOUD" else 0.08
+    df.loc[winter_off_peak_index, "Buy_Price"] = 0.37
+    winter_on_peak_index = pd.IndexSlice[[1, 2, 3, 4, 5, 10, 11, 12], :,
+                                         [17,18,19]]
+    df.loc[winter_on_peak_index, "Sell_Price"] = 0.39 if tou_type == "NEM2-TOUD" else 0.08
+    df.loc[winter_on_peak_index, "Buy_Price"] = 0.39
+    summer_off_peak_index = pd.IndexSlice[[6, 7, 8, 9], :,
+                                          [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,20,21,22,23]]
+    df.loc[summer_off_peak_index, "Sell_Price"] = 0.43 if tou_type == "NEM2-TOUD" else 0.08
+    df.loc[summer_off_peak_index, "Buy_Price"] = 0.43
+    summer_on_peak_index = pd.IndexSlice[[6, 7, 8, 9], :,
+                                         [17,18,19]]
+    df.loc[summer_on_peak_index, "Sell_Price"] = 0.49 if tou_type == "NEM2-TOUD" else 0.08
+    df.loc[summer_on_peak_index, "Buy_Price"] = 0.49
+else:
+    assert False, tou_type
+
+
+
+# Cost has to be computed hourly, the baseline applied monthly
+df["Net_Cost"] = 0
+df.loc[df["Net_Usage"] < 0, "Net_Cost"] = (df.loc[df["Net_Usage"] < 0, "Net_Usage"] *
+                                           df.loc[df["Net_Usage"] < 0, "Sell_Price"])
+df.loc[df["Net_Usage"] > 0, "Net_Cost"] = (df.loc[df["Net_Usage"] > 0, "Net_Usage"] *
+                                           df.loc[df["Net_Usage"] > 0, "Buy_Price"])
+
+
+df_daily = df.groupby(level=(0,-2)).sum()
+df_monthly = df.groupby(level=0).sum()
+
+df_monthly["Net_Cost"] -= (df_monthly["Net_Usage"]
+                           .clip(lower=0, upper=BASELINE_ALLOWANCE) *
+                           BASELINE_RATE)
+
+total_annual_kwh_gen = df["Total_Gen_ACKW"].sum()
+total_annual_kwh_gen_cur = df["Total_Gen_Cur_ACKW"].sum()
+total_annual_kwh_use = df["Used_ACKW"].sum()
+total_annual_kwh_net_use = df["Net_Usage"].sum()
+total_cost = df_monthly["Net_Cost"].sum()
+
+print("Rate plan:", tou_type)
+print("Panel layout:", panel_type)
+print("Inverter:", inverter_type)
+print(f"DC rating: {rated_dckw:,.2f} kW")
+print(f"Total generation: {total_annual_kwh_gen:,.2f} kWh/yr")
+print(f"Total usage:      {total_annual_kwh_use:,.2f} kWh/yr")
+print(f"Net usage:        {total_annual_kwh_net_use:,.2f} kWh/yr")
+# print(df_monthly["Net_Usage"])
+print(f"Total cost:       ${total_cost:,.2f}")
+# print(f"Total generation (current system): {total_annual_kwh_gen_cur:,.2f} kWh/yr")
+
+assert False
+
+plt.figure()
+df_monthly["Total_Gen_ACKW"].plot(color="orange", lw=2, label="Modeled System Gen")
+df_monthly["Used_ACKW"].plot(color="red", lw=3, label="2022 Usage")
+df_monthly["Net_Usage"].plot(color="black", lw=3, label="Net Usage")
+plt.legend()
+plt.show()
+
+
+if panel_type == "current":
+    label = "Modeled System Gen"
+else:
+    label = "New System Gen"
+df_daily["Total_Gen_ACKW"].plot(color="orange", lw=2, label=label)
+df_daily["Used_ACKW"].plot(color="black", lw=3, label="2022 Usage")
+df_daily["Total_Gen_Cur_ACKW"].plot(color="green", lw=1, label="2022 Gen Cur Sys")
+
+scale = df_daily["Total_Gen_ACKW"].mean() / df_daily["Total_Gen_Cur_ACKW"].mean()
+# print(f"New/Old Gen Scale: {scale:.3f}")
+# if system != "current":
+#     (df_daily["Total_Gen_Cur_ACKW"]*scale).plot(color="green", lw=1, ls="dashed",
+#                                                 label="2022 Gen Scaled")
+
+plt.legend()
+plt.ylabel("kWh/day")
+
+plt.figure()
+df_monthly["Total_Gen_ACKW"].plot()
+plt.show()
